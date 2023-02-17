8b95c4cf2715993f10aafd3834aa28f212aa1d29
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2023-02-17 16:57:33 -0400

    Botoes na Push Notification e Mensagem Foreground

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'react-native';
+
+import { Notification } from './src/components/Notification';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
-import OneSignal from 'react-native-onesignal';
+import OneSignal, { NotificationReceivedEvent, OSNotification }  from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
@@ -10,18 +12,33 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-import { tagUserEmailCreate } from './src/notifications/notificationsTags';
+import { tagUserInfoCreate } from './src/notifications/notificationsTags';
+import { useEffect } from 'react';
+// import { OSNotification } from 'react-native-onesignal';
 
 OneSignal.setAppId('010c26f8-8007-4a6c-832e-90ab8e772263');
 OneSignal.setEmail('carloshenriquepvh@hotmail.com');
 
-// OneSignal.setEmail('carloshenriquepvh@hotmail.com');
-
-
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+  tagUserInfoCreate();
 
-  tagUserEmailCreate('carloshenriquepvh@hotmail.com')
+  useEffect(() => {
+    const unsubscribe = OneSignal.setNotificationOpenedHandler((response) =>{
+      const {actionId} = response.action as any;
+
+      switch(actionId){
+        case '1': return console.log('Ver todas');
+        case '2': return console.log('Ver Amziade');
+        default: return console.log('Nenhum botão foi clicado');
+      }
+
+      // console.log('NOTIFICAÇÃO ABERTA!');
+    })
+
+    return () => unsubscribe;
+
+  },[])
 
   return (
     <NativeBaseProvider theme={THEME}>
