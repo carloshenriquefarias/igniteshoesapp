2f19196f4a3e286191ebf7e300eb0c17bb6b3b96
Carlos Henrique <you@example.comcarloshenriquepvh@hotmail.com>
2023-02-16 21:51:48 -0400

    Configurando o sistema para Push Notification

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import OneSignal from 'react-native-onesignal';
 
 import { Routes } from './src/routes';
 
@@ -9,8 +10,16 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
+
+OneSignal.setAppId('010c26f8-8007-4a6c-832e-90ab8e772263');
+OneSignal.setEmail('carloshenriquepvh@hotmail.com');
+
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('carloshenriquepvh@hotmail.com')
 
   return (
     <NativeBaseProvider theme={THEME}>
